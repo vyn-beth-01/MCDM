403a4737f80a6cf47d0dbac7097984ea85a5e5d2
vynguyen <beth1514147@gmail.com>
2025-09-21 22:57:46 +0700

    test relative dir

--- POM/page_src1.py
+++ POM/page_src1.py
@@ -5,7 +5,7 @@ Only calculate with src 1
 import pandas as pd
 # from  lib.callbacks import *
 # from lib.visualization_lib import * 
-from mcdm_func_lib.src.SAW import *
+from MCDM.mcdm_func_lib.src.SAW import *
 from ..mcdm_func_lib.src.TOPSIC import *
 from ..mcdm_func_lib.src.AHP import *
 from ..mcdm_func_lib.src.ELECTRE import *

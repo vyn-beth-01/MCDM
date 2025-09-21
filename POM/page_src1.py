1c4cc7f57dbc94577b50f61536209765a1c3d62e
vynguyen <beth1514147@gmail.com>
2025-09-21 23:06:22 +0700

    change dir of mcdm lib

--- POM/page_src1.py
+++ POM/page_src1.py
@@ -5,13 +5,13 @@ Only calculate with src 1
 import pandas as pd
 # from  lib.callbacks import *
 # from lib.visualization_lib import * 
-# from MCDM.mcdm_func_lib.src.SAW import *
-from ..mcdm_func_lib.src.TOPSIC import *
-from ..mcdm_func_lib.src.AHP import *
-from ..mcdm_func_lib.src.ELECTRE import *
-from ..mcdm_func_lib.src.formMain import *
-from ..mcdm_func_lib.src.PROMETHEE import *
-from ..mcdm_func_lib.src.VIKOR import *
+from mcdm_func_lib.src.SAW import *
+from mcdm_func_lib.src.TOPSIC import *
+from mcdm_func_lib.src.AHP import *
+from mcdm_func_lib.src.ELECTRE import *
+from mcdm_func_lib.src.formMain import *
+from mcdm_func_lib.src.PROMETHEE import *
+from mcdm_func_lib.src.VIKOR import *
 
 
 

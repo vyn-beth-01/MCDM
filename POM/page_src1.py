e31090eb67dd9af3576e8b8e01ba51d78a7fb3e4
vynguyen <beth1514147@gmail.com>
2025-09-21 22:47:39 +0700

    test

--- POM/page_src1.py
+++ POM/page_src1.py
@@ -1,11 +1,11 @@
 """
 Only calculate with src 1
 """
-import streamlit as st
+# import streamlit as st
 import pandas as pd
 # from  lib.callbacks import *
 # from lib.visualization_lib import * 
-from ..mcdm_func_lib.src.SAW import *
+from MCDM.mcdm_func_lib.src.SAW import *
 from ..mcdm_func_lib.src.TOPSIC import *
 from ..mcdm_func_lib.src.AHP import *
 from ..mcdm_func_lib.src.ELECTRE import *
@@ -15,9 +15,9 @@ from ..mcdm_func_lib.src.VIKOR import *
 
 
 
-# Main page content
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
+# # Main page content
+# st.markdown("# Main page 🎈")
+# st.sidebar.markdown("# Main page 🎈")
 
 
 

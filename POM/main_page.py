60442178bef91e813f64d6ff699b55bfa2f7ba22
vynguyen <beth1514147@gmail.com>
2025-09-21 22:22:43 +0700

    change dir for logo

--- POM/main_page.py
+++ POM/main_page.py
@@ -10,7 +10,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.sidebar.image('../figures/Logo-HCMIU.svg.png')
+st.sidebar.image('Logo-HCMIU.svg.png')
 st.sidebar.title('Subject: MCDM')
 
 
